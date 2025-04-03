6aa82f0d14322d1bed5c29d0dbfe0d6ddd9460a6
NukeMinecart <145407522+NukeMinecart@users.noreply.github.com>
2025-04-03 17:44:13 -0500

    add type checking to AnnotationProcessor and rework documentation

--- lib/src/main/java/badgerlog/AnnotationProcessor.java
+++ lib/src/main/java/badgerlog/AnnotationProcessor.java
@@ -1,5 +1,7 @@
 package badgerlog;
 
+import badgerlog.entry.Entry;
+import badgerlog.networktables.mappings.MappingType;
 import com.google.auto.service.AutoService;
 
 import javax.annotation.processing.*;
@@ -8,18 +10,19 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
 import javax.tools.Diagnostic;
 import java.util.Set;
 
 /**
- * Annotation processor for both {@link badgerlog.entry.Entry} and {@link badgerlog.networktables.mappings.MappingType}
+ * Annotation processor for BadgerLog. Verifies that each field annotated with {@link Entry} or {@link MappingType} is static, non-final, and conforms to the type restriction
  */
 @SupportedAnnotationTypes(
         "badgerlog.*")
 @SupportedSourceVersion(SourceVersion.RELEASE_17)
 @AutoService(Processor.class)
-public class AnnotationProcessor extends AbstractProcessor {
-
+public final class AnnotationProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         for (TypeElement annotation : annotations) {
@@ -32,11 +35,22 @@ public class AnnotationProcessor extends AbstractProcessor {
                 var modifiers = variableElement.getModifiers();
                 var name = variableElement.getSimpleName().toString();
                 var className = variableElement.getEnclosingElement().toString();
-                
+
                 int flags = 0;
                 if (!modifiers.contains(Modifier.STATIC)) flags += 1;
                 if (modifiers.contains(Modifier.FINAL)) flags += 2;
-                    
+
+                Types typeUtils = processingEnv.getTypeUtils();
+                Elements elementUtils = processingEnv.getElementUtils();
+
+                var mappingType = elementUtils.getTypeElement("badgerlog.networktables.mappings.Mapping");
+                var mappingAnnotation = elementUtils.getTypeElement("badgerlog.networktables.mappings.MappingType");
+
+                if (typeUtils.isSameType(annotation.asType(), mappingAnnotation.asType()))
+                    if (!typeUtils.isSameType(variableElement.asType(), mappingType.asType()))
+                        printErrorMessage(String.format("%s in %s must be of type Mapping if annotated with MappingType", name, className), element);
+
+
                 switch (flags) {
                     case 1:
                         printErrorMessage(String.format("%s in %s must be static", name, className), element);
@@ -52,7 +66,7 @@ public class AnnotationProcessor extends AbstractProcessor {
         }
         return true;
     }
-    
+
     private void printErrorMessage(String message, Element element) {
         processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, message, element);
     }
