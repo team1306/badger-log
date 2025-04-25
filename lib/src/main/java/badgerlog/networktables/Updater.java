d11c5e15fc0a4a18144d736de68cc0c66a014eb3
NukeMinecart <145407522+NukeMinecart@users.noreply.github.com>
2025-04-25 15:04:50 -0500

    Updater javadoc

--- lib/src/main/java/badgerlog/networktables/Updater.java
+++ lib/src/main/java/badgerlog/networktables/Updater.java
@@ -1,11 +1,13 @@
 package badgerlog.networktables;
 
 /**
- * Interface representing a type that can be updated from NetworkTables.
+ * Interface defining objects capable of synchronizing data with NetworkTables.
+ * Implementations handle either publishing data to or retrieving data from NetworkTables entries.
  */
 public interface Updater {
     /**
-     * Publishes or gets a value from NetworkTables
+     * Performs a synchronization operation with NetworkTables. This may involve either
+     * pushing local values to the network or updating local state with NetworkTables values.
      */
     void update();
 }
