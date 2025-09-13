e63b59190379028fe79016fa0f81e87d6f75dd9e
NukeMinecart <145407522+NukeMinecart@users.noreply.github.com>
2025-09-12 21:45:43 -0500

    correct the testing to have updating fields

--- test-project/src/main/java/frc/robot/FieldsTest.java
+++ test-project/src/main/java/frc/robot/FieldsTest.java
@@ -1,16 +1,23 @@
 package frc.robot;
 
-import badgerlog.annotations.*;
+import badgerlog.annotations.AutoGenerateStruct;
+import badgerlog.annotations.Entry;
+import badgerlog.annotations.EntryType;
+import badgerlog.annotations.Key;
+import badgerlog.annotations.Struct;
+import badgerlog.annotations.StructType;
+import badgerlog.annotations.UnitConversion;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.measure.Distance;
 
 import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Millimeter;
 
 public class FieldsTest implements Testing {
 
     @Entry(EntryType.PUBLISHER)
-    public int basicInteger = 1;
+    public static int basicInteger = 1;
 
     @Entry(EntryType.SUBSCRIBER)
     @Key("Auto/AutoWaitTime")
@@ -31,6 +38,7 @@ public class FieldsTest implements Testing {
     @Entry(EntryType.SUBSCRIBER)
     @UnitConversion(value = "in", converterId = "translation")
     @UnitConversion(value = "radian", converterId = "rotation")
+    @Struct(StructType.MAPPING)
     public Pose2d robotPose = Pose2d.kZero;
 
     @Entry(EntryType.PUBLISHER)
@@ -55,6 +63,9 @@ public class FieldsTest implements Testing {
 
     @Override
     public void update() {
-
+        basicInteger ++;
+        height = Millimeter.of(2).plus(height);
+        rotation2d = Rotation2d.fromDegrees(1).plus(rotation2d);
+        System.out.println(robotPose);
     }
 }
